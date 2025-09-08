a93f5611e4b7b08494498a1a7b76a599fe8f928d
Ceil-Studio <ceiilciiuz@gmail.com>
2025-09-08 09:15:14 +0200

    Update rules.sh

--- rules.sh
+++ rules.sh
@@ -4,22 +4,16 @@ sysctl -w net.ipv4.ip_forward=1
 
 sudo iptables -t nat -N REDSOCKS 2>/dev/null
 
-# Nettoyer les anciennes règles
 sudo iptables -t nat -F REDSOCKS
 sudo iptables -t nat -F OUTPUT
 
-# Garder les exclusions localhost
 sudo iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
 
-# Exclure panel (HTTP seulement)
 sudo iptables -t nat -A REDSOCKS -p tcp --dport 80 -j RETURN
 
-# Exclure GMod
 sudo iptables -t nat -A REDSOCKS -p tcp --dport 27015 -j RETURN
 
-# Tout le reste redirigé vers redsocks (port 12345)
 sudo iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
 
-# Appliquer la chaîne REDSOCKS sur tout le trafic sortant TCP
 sudo iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
 
