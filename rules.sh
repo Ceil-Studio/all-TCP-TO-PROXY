fdfb5d565bb028911e8b5741508e179e535be67d
Ceil-Studio <ceiilciiuz@gmail.com>
2025-09-08 09:30:50 +0200

    Update rules.sh

--- rules.sh
+++ rules.sh
@@ -17,3 +17,9 @@ sudo iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
 
 sudo iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
 
+# regle de base qui fonctionne :
+
+sudo iptables -t nat -N REDSOCKS
+sudo iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
+sudo iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
+sudo iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
